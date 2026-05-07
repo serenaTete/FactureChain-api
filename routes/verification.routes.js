f79f882cebfc6f1ae005eb08d0dc6a90d14a66e6
serenaTete <tetedeloka@gmail.com>
2026-05-07 10:30:28 +0200

    fcbackend1.3

--- routes/verification.routes.js
+++ routes/verification.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import * as ctrl from "../controllers/verification.controller.js";
+import {authMiddleware} from "../middlewares/auth.js";
+import {roleMiddleware} from "../middlewares/role.middleware.js";
 
 const router = express.Router();
 
-router.post("/", ctrl.verify);
+router.post("/",
+    authMiddleware,
+    ctrl.verify);
 
 export default router;
